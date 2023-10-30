2d86b18f7d4e7e214ded767d6b32a95e62fab8aa
isurojit <iamsurojit@hotmail.com>
2023-10-30 10:46:31 +0530

    Progress bar ui and calories ui indicator added

--- js/app.js
+++ js/app.js
@@ -60,10 +60,30 @@ class CalorieTracker {
 
   _displayCaloriesRemaining() {
     const caloriesRemainingEl = document.getElementById("calories-remaining");
+    const progressEl = document.getElementById("calorie-progress");
 
     const remaning = this._calorieLimit - this._totalCalories;
 
     caloriesRemainingEl.innerHTML = remaning;
+
+    if (remaning <= 0) {
+      caloriesRemainingEl.parentElement.parentElement.classList.remove(
+        "bg-light"
+      );
+      caloriesRemainingEl.parentElement.parentElement.classList.add(
+        "bg-danger"
+      );
+      progressEl.classList.remove("bg-success");
+      progressEl.classList.add("bg-danger");
+    } else {
+      caloriesRemainingEl.parentElement.parentElement.classList.remove(
+        "bg-danger"
+      );
+      caloriesRemainingEl.parentElement.parentElement.classList.add("bg-light");
+
+      progressEl.classList.add("bg-success");
+      progressEl.classList.remove("bg-danger");
+    }
   }
 
   _displayCaloriesprogress() {
